b383d5a3d62a4b82a955768855ba2e6b2efa0dab
santy-estrada <santiago.estrada6@eia.edu.co>
2023-10-01 00:06:12 -0500

    Jarra y Problema Listos

--- Jarras/src/Solucion/Problema.java
+++ Jarras/src/Solucion/Problema.java
@@ -1,22 +1,24 @@
 package Solucion;
 
-public class Problema <E extends Comparable<E>>{
+public class Problema implements Comparable<Problema>{
 	
 	private Jarra jarra1;
 	private Jarra jarra2;
 	private int objetivo;
 	
-	Problema(int VMax1, int VMax2, int objetivo){
+	public Problema(int VMax1, int VMax2, int objetivo){
+		this.jarra1 = new Jarra(VMax1);
+		this.jarra2 = new Jarra(VMax2);
 		this.objetivo= objetivo;
 	}
 	
 	public int getCant1() {
-		int volumenActual1= getVolumenActual();
+		int volumenActual1= jarra1.getVolumenActual();
 		return volumenActual1;
 	}
 	
 	public int getCant2() {
-		int volumenActual2= getVolumenActual();
+		int volumenActual2= jarra2.getVolumenActual();
 		return volumenActual2;
 	}
 
@@ -42,6 +44,21 @@ public class Problema <E extends Comparable<E>>{
 
 	public void setObjetivo(int objetivo) {
 		this.objetivo = objetivo;
+	}
+
+	@Override
+	public String toString() {
+		return "Problema [jarra1=" + jarra1 + ", jarra2=" + jarra2 + ", objetivo=" + objetivo + "]";
+	}
+
+
+	@Override
+	public int compareTo(Problema p) {
+		// TODO Auto-generated method stub
+		if(jarra1.getVolumenActual() == p.getCant1() && jarra2.getVolumenActual() == p.getCant2()) {
+			return 0;
+		}
+		return 1;
 	}
 	
 	
