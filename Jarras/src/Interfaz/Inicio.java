419505e9fba74cfcdf4e393d3aeeb9d143d07dfc
santy-estrada <santiago.estrada6@eia.edu.co>
2023-10-03 11:18:10 -0500

    Inicio de interfaz

---
+++ Jarras/src/Interfaz/Inicio.java
@@ -0,0 +1,103 @@
+package Interfaz;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.ImageIcon;
+
+public class Inicio {
+
+	private JFrame frame;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Inicio window = new Inicio();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Inicio() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 574, 554);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Problema de las Jarras");
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 25));
+		lblNewLabel.setEnabled(false);
+		lblNewLabel.setBounds(21, 32, 306, 45);
+		frame.getContentPane().add(lblNewLabel);
+		
+		textField = new JTextField();
+		textField.setBounds(21, 125, 131, 19);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblNewLabel_1 = new JLabel("Jarra 1:");
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblNewLabel_1.setBounds(21, 106, 92, 13);
+		frame.getContentPane().add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_1_1 = new JLabel("Jarra 2:");
+		lblNewLabel_1_1.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblNewLabel_1_1.setBounds(335, 106, 92, 13);
+		frame.getContentPane().add(lblNewLabel_1_1);
+		
+		textField_1 = new JTextField();
+		textField_1.setColumns(10);
+		textField_1.setBounds(335, 125, 131, 19);
+		frame.getContentPane().add(textField_1);
+		
+		JLabel lblNewLabel_1_2 = new JLabel("Objetivo:");
+		lblNewLabel_1_2.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblNewLabel_1_2.setBounds(21, 193, 92, 13);
+		frame.getContentPane().add(lblNewLabel_1_2);
+		
+		textField_2 = new JTextField();
+		textField_2.setColumns(10);
+		textField_2.setBounds(21, 212, 131, 19);
+		frame.getContentPane().add(textField_2);
+		
+		JButton btnNewButton = new JButton("SOLUCIONAR");
+		btnNewButton.setEnabled(false);
+		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		btnNewButton.setBounds(335, 174, 141, 51);
+		frame.getContentPane().add(btnNewButton);
+		
+		JLabel lblNewLabel_2 = new JLabel("");
+		lblNewLabel_2.setIcon(new ImageIcon(Inicio.class.getResource("/Interfaz/giphy.gif")));
+		lblNewLabel_2.setBounds(133, 244, 294, 273);
+		frame.getContentPane().add(lblNewLabel_2);
+		/*
+		Icon imgIcon = new ImageIcon(this.getClass().getResource("ajax-loader.gif"));
+		JLabel label = new JLabel(imgIcon);
+		label.setBounds(668, 43, 46, 14); // You can use your own values
+		frame.getContentPane().add(label);*/
+	}
+}
